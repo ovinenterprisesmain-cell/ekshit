b773948d1dd0196e727e07cc978635124990e4e3
ovinenterprisesmain-cell <ovinenterprises.main@gmail.com>
2025-12-23 14:13:19 +0530

    Update server.py

--- python/server.py
+++ python/server.py
@@ -264,5 +264,5 @@ def run_scraper_in_background():
 # ========================
 if __name__ == "__main__":
     threading.Thread(target=run_scraper_in_background, daemon=True).start()
-    print("🚀 Flask API running at http://127.0.0.1:5000/api/data")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
